5e5c2097f9bf48c6ba37cd946e6ece2cdd683ffe
posinski1210 <osinski210@gmail.com>
2024-05-26 10:16:52 +0200

    orders, clients tyle to robilem ze juz nie pamietam co tam jest xd

---
+++ newbackend/src/main/java/com/kul/newbackend/entities/Client.java
@@ -0,0 +1,37 @@
+package com.kul.newbackend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Table(name = "clients")
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "client_id")
+    private Long clientId;
+
+    @Column(name = "client_name")
+    private String clientName;
+
+    @Column(name = "nip_number",unique = true)
+    private String nipNumber;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "client_email")
+    private String clientEmail;
+
+
+}
